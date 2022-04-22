16d9521e838c6232d32213e4927bda11393388ef
erdincDonmez <erdinc_donmez@yahoo.com>
2022-04-22 11:51:30 +0000

    Turtle eklendi.

--- P07_turtle4.py
+++ P07_turtle4.py
@@ -1,6 +1,7 @@
 import turtle
-for b in range(8) :
+turtle.speed(100)
+for b in range(12) :
     for a in range(4):
         turtle.forward(100)
-        turtle.right(90)
+        turtle.right(92)
     turtle.right(45)
