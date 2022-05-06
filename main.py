ccb26ec2569e501c524799d565f53b98ddaf612f
erdincDonmez <erdinc_donmez@yahoo.com>
2022-05-06 20:30:00 +0000

    OOP constructor yapici fonksiyon

--- main.py
+++ main.py
@@ -1,38 +1,46 @@
-# insertion sort
-dizi = [22,27,16,2,18,6]
-for i in range(1, len(dizi)):
-  deger = dizi[i]
-  j = i-1
-  while j >=0 and deger<dizi[j] :
-    dizi[j+1] = dizi[j]
-    j -= 1
-    dizi[j+1] = deger
-print(dizi)
+class araclar():
+    tip = "arac"
+    uretici = "mercedes"
+    def __init__(self, gelenTip="tip tanımsız",gelenUretici="bilinmiyor"):# fonksiyon çağırılınca çalışacak fonksiyon
+        self.tip=gelenTip # __init__ tanımına constructor yani yapıcı fonksiyon deniyor.
+        self.uretici=gelenUretici # yapıcı fonksiyonlar fonksiyon çalışınca standar bir şey üretmek
+    def listele(self): 
+        print("Araç tipi     :", self.tip)
+        print("Araç ureticisi:", self.uretici,"\n")
+        
+    def Ekle(self,eklemeYeri="Eklenecek yer default SQL"): 
+        print("Araç Ekleme işlemi")
+        print("==================")
+        print("Araç tipi     :", self.tip)
+        print("Araç ureticisi:", self.uretici)
+        print("Ekleme yeri   :", eklemeYeri,"\n")
+        print("Ekleme tamam")
 
-"""
-- Yukarı verilen dizinin sort türüne göre aşamalarını yazınız.
+print("araclar sınıfı için")
+#araclar.listele() # hata
+print("Araç tipi     :", araclar.tip)
+print("Araç ureticisi:", araclar.uretici)
 
-[22,27,16, 2,18, 6] 
-[ 2,22,27,16,18, 6] 
-[ 2, 6,22,27,16,18] 
-[ 2, 6,16,22,27,18] 
-[ 2, 6,16,18,22,27]
-[ 2, 6,16,18,22,27]
-[ 2, 6,16,18,22,27]
+arac1=araclar() # Sınıftan Ornekleme
 
+print("\narac1 Nesne örneği için")
+arac1.listele()
 
-- Big-O gösterimini yazınız.
+arac2=araclar() # Sınıftan Ornekleme
+arac2.tip="Araba"
+arac2.uretici="volvo"
+print("\narac2 Nesne örneği için")
+arac2.listele()
+arac2.kapiSayisi=4 # nesneye sonradan özellik atayabiliriz.
+print("Arac kapi sayisi:",arac2.kapiSayisi,"\n")
+arac2.listele() # kapi sayısını göstermez
 
-n^2 = 36
+print("\narac3 Nesne örneği için")
+arac3=araclar("Kamyon","BMW") # bunu init yapıcı fonksiyonu sayesinde yapabildik.
+arac3.listele()
 
+print("\narac4 Nesne örneği için")
+arac4=araclar("Bisiklet","Carraro") 
+print (arac4.Ekle())# burada Ekle metodunu kullandık
+print (arac4.Ekle("access"))# burada Ekle metodunu kullandık
 
-- Time Complexity:
-
-En iyi durum  : O(n)   (Best case: Dizinin sıralı olması durumu)
-En kötü durum : O(n^2) (Worst case: Aradığımız sayının sonda olması)
-Ortalama      : O(n^2) (Average case: Aradığımız sayının ortada olması)
-
-- Dizi sıralandıktan sonra 18 sayısı hangi case kapsamına girer? 
-
-Ortalama (Avarege)
-"""
