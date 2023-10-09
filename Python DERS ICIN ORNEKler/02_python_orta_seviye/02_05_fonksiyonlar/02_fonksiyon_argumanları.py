f494898c401b755217ee0ce7ad8e842c91171723
erdincdonmez <erdinc_donmez@yahoo.com>
2023-10-09 09:34:13 +0300

    desktop.ini

---
+++ Python DERS ICIN ORNEKler/02_python_orta_seviye/02_05_fonksiyonlar/02_fonksiyon_argumanları.py
@@ -0,0 +1,5 @@
+def bol(s1, s2):
+	print (s1/s2)
+bol(8,2)
+bol(s2=6,s1=2)
+
--- Python DERS ICIN ORNEKler/07_recurtion_4saat/recursive-1 faktoriyel.py
+++
@@ -1,8 +0,0 @@
-#Faktöriyel bir sayıya kadar olan sayıların çarpımıdır.
-def faktoriyel(x):
-    if x == 1:
-        return 1
-    else:
-        return (x * faktoriyel(x-1))
-sayi = 6
-print(sayi, "sayısının faktöriyeli", faktoriyel(sayi))
