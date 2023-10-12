21ab79359daf6f33e94db0c6bfff6b284961a643
erdincdonmez <erdinc_donmez@yahoo.com>
2023-10-12 16:17:33 +0300

    mySQL

--- Python DERS ICIN ORNEKler/07_veritabani_mysql/02_veritabani olusturma.py
+++ Python DERS ICIN ORNEKler/07_veritabani_mysql/02_veritabani olusturma.py
@@ -3,7 +3,7 @@ try:
   mydb = mysql.connector.connect(
     host="localhost", # Community Server yerel bilgisayara kurulup çalıştırılmışsa localhost olacak.
     user="root", # MySQL Community Server indirip kurunca özel bir kullanıcı adı verilmemişse root olacak
-    password="qwe321" # MySQL Community Server kurulumu aşamasında size sorduğu şifre
+    password="123" # MySQL Community Server kurulumu aşamasında size sorduğu şifre
   )
   print("Bağlantı tamam:")
   print(mydb)
--- Python DERS ICIN ORNEKler/07_veritabani_mysql/03_veritabanları_listesi.py
+++ Python DERS ICIN ORNEKler/07_veritabani_mysql/03_veritabanları_listesi.py
@@ -3,7 +3,7 @@ import mysql.connector
 mydb = mysql.connector.connect(
   host="localhost",
   user="root",
-  password="qwe321"
+  password="123"
 )
 
 mycursor = mydb.cursor()
