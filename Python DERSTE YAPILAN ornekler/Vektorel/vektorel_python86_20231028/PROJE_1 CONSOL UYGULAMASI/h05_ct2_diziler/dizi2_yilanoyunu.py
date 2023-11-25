d41f4809b18a3d87bc261402ba0e19c28f55bc52
erdincDonmez <erdinc_donmez@yahoo.com>
2023-11-26 01:13:28 +0300

    Add files via upload

--- Python DERSTE YAPILAN ornekler/Vektorel/vektorel_python86_20231028/PROJE_1 CONSOL UYGULAMASI/h05_ct2_diziler/dizi2_yilanoyunu.py
+++ Python DERSTE YAPILAN ornekler/Vektorel/vektorel_python86_20231028/PROJE_1 CONSOL UYGULAMASI/h05_ct2_diziler/dizi2_yilanoyunu.py
@@ -1,32 +1,50 @@
-def tahtaOlustur(boyuta,karaktera):
+def tahtaOlustur(boyutT,bosKarakter,yerX,yerY,karakterYilan):
+    tahta = []
+    for a in range(boyutT):
+        sira = []
+        for b in range(boyutT):
+            if a==yerX and b==yerY : sira.append(karakterYilan)
+            else: sira.append(bosKarakter)
+        tahta.append(sira)
 
-    # tahta = []
-    # tahta = [[".",".",".",".",".",".",".",],
-    #         [".",".",".",".",".",".",".",],
-    #         [".",".",".",".",".",".",".",],
-    #         [".",".",".",".",".",".",".",]
-    #         ]
-
-    
-    for a in range(2):
-        for b in range(2):
-            tahta.insert([a][b],karaktera)
-    
-    # for a in range(boyuta):
-    #     for b in range(boyuta):
-    #         print(tahta[a][b])
-
-    # for a in range(boyuta):
-    #     print(karaktera*boyuta)
-
+    Satir =""
+    for a in range(boyutT):
+        for b in range(boyutT):
+            Satir += tahta[a][b]
+        print (Satir)
+        Satir =""
+    print("\n")
 import random
-def karakterYerlestir(gelen,boyutx):
-    tahtaOlustur(boyutx,".")
-    x = random.randint(0,boyut)
-    y = random.randint(0,boyut)
-    print(x,y,gelen)
+boyut = 10
+karakter = "░"
+x = random.randint(0,boyut)
+y = random.randint(0,boyut)
+yilanKarakteri = "©"
+tahtaOlustur(boyut,karakter,x,y,yilanKarakteri)
 
+# Basılan tuşları algılama
+# pip install keyboard
+import keyboard
 
+def on_key_event(event):
+    global x
+    global y
+    if event.name == 'up' or event.name == 'down' or event.name == 'left' or event.name == 'right':
+        # print(f"Girilen yön tuşu: {event.name}")
+        if event.name == "right": 
+            y+=1
+        if event.name == "left": 
+            y-=1
+        if event.name == "up": 
+            x-=1
+        if event.name == "down": 
+            x+=1
+        if x>boyut : x=1
+        if x<0: x=boyut
+        if y>boyut : y=1
+        if y<0 : y = boyut
+        tahtaOlustur(boyut,karakter,x,y,yilanKarakteri)
 
-boyut = 15
-karakterYerlestir("O",boyut)
+keyboard.on_release(on_key_event)
+
+keyboard.wait('esc')  # Programın çalışmasını sonlandırmak için "esc" tuşuna basın
