a18200f0fdfeb88e49b8861856aa70f313210c58
erdincDonmez <erdinc_donmez@yahoo.com>
2023-12-27 21:05:44 +0300

    Add files via upload

---
+++ Python DERSTE YAPILAN ornekler/Vektorel/vektorel_python88_20231225/h01_pt/h01_g1_d1_giris/1-print.py
@@ -0,0 +1,6 @@
+adi = "Mustafa"
+soyAdi = "ALPER"
+
+print("1"+"3", adi, soyAdi)
+print("1"+"3", "adi: ",adi, soyAdi)
+print(1+3, adi, soyAdi)
---
+++ Python DERSTE YAPILAN ornekler/Vektorel/vektorel_python88_20231225/h01_pt/h01_g2_d2_input/2-input.py
@@ -0,0 +1,7 @@
+adi = input("Adı : ")
+soyAdi = input("Soyadı : ")
+no = input("Nosu :")
+
+ print("Demek adın",adi,", \nmemnun oldum",adi,soyAdi)
+print(no)
+
