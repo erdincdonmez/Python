914cd83689dccef304a6021a7dc59e8917e6beef
erdincDonmez <erdinc_donmez@yahoo.com>
2023-12-29 21:25:32 +0300

    grup 88, h1, g3

---
+++ Python DERSTE YAPILAN ornekler/Vektorel/vektorel_python88_20231225/h01_pt/1_CONSOLE_UYGULAMALARI/h00_tanitim_dersi/tanitim01.py
@@ -0,0 +1,16 @@
+print("╔═══════════════════╗")
+print("║  PYTHON DERSİ     ║")
+print("║                   ║")
+print("║ 1-Hesap makinesi  ║")
+print("║ 2-Hesap makinesi  ║")
+print("║ 3-Hesap makinesi  ║")
+print("║ 4-Hesap makinesi  ║")
+print("║ Seçiminiz nedir?  ║")
+print("╚═══════════════════╝")
+
+# 201 ╔
+# 205 ═
+# 187 ╗
+# 186 ║
+# 200 ╚
+# 188 ╝
---
+++ Python DERSTE YAPILAN ornekler/Vektorel/vektorel_python88_20231225/h01_pt/1_CONSOLE_UYGULAMALARI/hesapmakinesi/hesaplamalar.py
@@ -0,0 +1,19 @@
+def hmmenu():
+    #print("╔"+"═"*20+"╗")
+    print("╔═════════════════════╗")
+    print("║\033[1;31;40m   HESAP MAKİNESİ  \033[1;32;40m  ║")
+    print("║                     ║")
+    print("║  1-Toplama          ║")
+    print("║  2-Çıkarma          ║")
+    print("║  3-Çarpma           ║")
+    print("║  4-Bölme            ║")
+    print("║                     ║")
+    print("║    Seçimiz nedir?   ║")
+    print("╚═════════════════════╝")
+    secim = input()
+    # 201 ╔
+    # 205 ═
+    # 187 ╗
+    # 186 ║
+    # 200 ╚
+    # 188 ╝
