dcb8cf0b011b2535356c362dbb2129010439cba3
erdincDonmez <erdinc_donmez@yahoo.com>
2023-12-31 22:14:33 +0300

    grup 88 düzenleme

---
+++ Python DERSTE YAPILAN ornekler/Vektorel/vektorel_python88_20231225/1_CONSOLE_UYGULAMALARI/01_13/1_input.py
@@ -0,0 +1,4 @@
+input() # input bu şekilde kullanılırsa bekletme gibi kullanılabilir.
+aa = input() # diyez işareti sonrasında açıklama ifadeleri yazılabilir.
+bb = input("Bir ad girin: ")
+print ("Girdiğiniz ad: ", bb)
