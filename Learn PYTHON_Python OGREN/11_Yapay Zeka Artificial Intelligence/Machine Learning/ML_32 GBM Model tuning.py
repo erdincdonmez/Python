d27c84bc039e8dedcdfb19ed8e6dbee5890903bb
erdincDonmez <erdinc_donmez@yahoo.com>
2024-07-12 23:49:34 +0300

    GBM Modeli

--- Learn PYTHON_Python OGREN/11_Yapay Zeka Artificial Intelligence/Machine Learning/ML_32 GBM Model tuning.py
+++ Learn PYTHON_Python OGREN/11_Yapay Zeka Artificial Intelligence/Machine Learning/ML_32 GBM Model tuning.py
@@ -1,6 +1,5 @@
 # GBM Gradient Boosting Machine
-import numpy as np
-import pandas as pd
+import numpy as np; import pandas as pd
 from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.metrics import mean_squared_error, r2_score
 import matplotlib.pyplot as plt
