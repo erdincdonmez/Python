834c66ab50b32f5230558190286d42c3348dbe5a
erdincDonmez <erdinc_donmez@yahoo.com>
2024-07-14 23:36:27 +0300

    CART Classification and Regression Tree

---
+++ Learn PYTHON_Python OGREN/11_Yapay Zeka Artificial Intelligence/Machine Learning/ML_49 CART model tahmin.py
@@ -0,0 +1,37 @@
+# CART ile sınıflandırma problemlerinde Model ve tahmin
+import numpy as np; import pandas as pd
+import statsmodels.api as sm
+import matplotlib.pyplot as plt
+from sklearn.metrics import confusion_matrix, accuracy_score, mean_squared_error, r2_score, roc_auc_score, roc_curve, classification_report
+import warnings; warnings.filterwarnings("ignore", category=FutureWarning) # Hatalardan kaçınmak için
+warnings.filterwarnings("ignore", category=DeprecationWarning) # Hatalardan kaçınmak için
+from sklearn.model_selection import train_test_split, GridSearchCV, cross_val_score
+from sklearn.tree import DecisionTreeClassifier
+
+#! VERİLERİN YÜKLENİP HAZIRLANMASI
+df = pd.read_csv("dataframes/diabetes.csv") # Şeker hastalığı ile ilgili veri setimizi içe aktarıyoruz
+print("\n\ndf.head()\n",df.head())
+print('\n\ndf["Outcome"].value_counts()\n',df["Outcome"].value_counts())
+print('\n\ndf.describe().T\n',df.describe().T)
+
+y = df["Outcome"]
+X = df.drop(["Outcome"],axis=1)
+
+print('\n\ny.head()\n',y.head())
+print('\n\nX.head()\n',X.head())
+
+# Verileri eğitim ve test olarak bölme
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.30,random_state=42)
+
+#! MODEL KURMA 
+cart_model = DecisionTreeClassifier().fit(X_train,y_train)
+print("\n\ncart_model.get_params()\n",cart_model.get_params())
+y_pred = cart_model.predict(X_test)
+
+#! İlkel test hatası 
+# Sınıflandırma problemlerinin tahmin başarısını ölçmek için accuracy_score metriğini kullanacağız.
+# accuracy_score ne kadar büyükse model o kadar başarılı demektir.
+print("\n\naccuracy_score(y_test, y_pred)",accuracy_score(y_test, y_pred))
+# print("\n\nSınıflandırma raporu: \n",classification_report(y_test,y_pred))
+
+
