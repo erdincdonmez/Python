b3385e0c20f796c17b8995eba562ac7369012c34
erdincDonmez <erdinc_donmez@yahoo.com>
2024-07-16 23:05:58 +0300

    k-means

--- Learn PYTHON_Python OGREN/11_Yapay Zeka Artificial Intelligence/Machine Learning/ML_62 K-Means ile clustering.py
+++ Learn PYTHON_Python OGREN/11_Yapay Zeka Artificial Intelligence/Machine Learning/ML_62 K-Means ile clustering.py
@@ -1,3 +1,6 @@
 import numpy as np; import pandas as pd
 import seaborn as sns; import matplotlib.pyplot as plt
-https://gelecegiyazanlar.turkcell.com.tr/konu/egitim/makine-ogrenmesi-501/k-means-uygulama
+from sklearn.cluster import KMeans
+import warnings; warnings.filterwarnings("ignore", category=FutureWarning
+warnings.filterwarnings("ignore", category=DeprecationWarning)
+
