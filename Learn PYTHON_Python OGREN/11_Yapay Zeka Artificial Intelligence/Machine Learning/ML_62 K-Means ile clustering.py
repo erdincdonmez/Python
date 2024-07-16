2c35cf4be49663f599c9708cb043fb3499f5954f
erdincdonmez <erdinc_donmez@yahoo.com>
2024-07-16 17:18:27 +0300

    K-means ile clustering

---
+++ Learn PYTHON_Python OGREN/11_Yapay Zeka Artificial Intelligence/Machine Learning/ML_62 K-Means ile clustering.py
@@ -0,0 +1,3 @@
+import numpy as np; import pandas as pd
+import seaborn as sns; import matplotlib.pyplot as plt
+https://gelecegiyazanlar.turkcell.com.tr/konu/egitim/makine-ogrenmesi-501/k-means-uygulama
